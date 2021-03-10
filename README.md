4b2e4fcb0365244e88f3c904a0722d5194de4231
Christian Baumann <chbauman@student.ethz.ch>
2021-03-10 19:02:45 +0100

    Run the scraper locally and updated readme

--- README.md
+++ README.md
@@ -3,3 +3,25 @@
 Web-app für Visualisierung von WG-Zimmer Angeboten.
 
 Verfügbar unter: https://chbauman.github.io/wg-map/
+
+## Development
+
+For debugging / testing run
+
+```
+http-server
+```
+
+To run the scraper written in python, use the following command.
+
+```
+python ./main.py --update --force
+```
+
+Installing a virtual environment:
+
+```
+python -m venv venv
+./venv/Scripts/Activate.ps1
+python -m pip install -r requerements.txt
+```
--- main.py
+++ main.py
@@ -50,10 +50,9 @@ def init_driver():
     chrome_options = Options()
     chrome_options.add_argument("--headless")
     chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--disable-gpu")
     chrome_options.add_argument("--disable-dev-shm-usage")
-    driver = webdriver.Chrome(
-        ChromeDriverManager().install(), chrome_options=chrome_options
-    )
+    driver = webdriver.Chrome(ChromeDriverManager().install(), options=chrome_options)
     driver.set_page_load_timeout(100)
     return driver
 
@@ -114,7 +113,7 @@ def find_all_in(state: str, driver, verbose: bool = True):
             time.sleep(1)
         except NoSuchElementException:
             if verbose:
-                print(f"No next page!")
+                print("No next page!")
             page_available = False
 
     return all_items
